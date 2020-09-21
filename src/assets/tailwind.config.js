5facb5232c5ba16ea9f3469311c3fea84b90e367
karen <karenlukitooo@gmail.com>
2020-09-21 17:30:26 +0700

    Add tailwindcss ui

---
+++ src/assets/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    colors: {
+      indigo: '#5c6ac4',
+      blue: '#007ace',
+      red: '#de3618',
+      red500: '#F56565',
+      orange400: '#f6ad55',
+      blue400: '#63B3ED',
+      blue500: '#4299E1',
+    }
+
+  },
+  variants: {
+
+  },
+  plugins: [
+    
+  ],
+}
